c81e7e91dcd98c1779fc3beec8a5eb7a535e6661
5ahil <withshahil@gmail.com>
2018-05-16 00:51:31 +0530

    test2

--- Two Pointers/Tricks/Max_Continuous_Series_of_1s.cpp
+++ Two Pointers/Tricks/Max_Continuous_Series_of_1s.cpp
@@ -1,4 +1,6 @@
-/*You are given with an array of 1s and 0s. And you are given with an integer M, which signifies number of flips allowed.
+/*
+Modified a bit--Sahil
+You are given with an array of 1s and 0s. And you are given with an integer M, which signifies number of flips allowed.
 Find the position of zeros which when flipped will produce maximum continuous series of 1s.
 
 For this problem, return the indices of maximum continuous series of 1s in order.
